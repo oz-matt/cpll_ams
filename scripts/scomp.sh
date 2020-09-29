45eee48e3c38b9ca4924ddfa8ee6188c9c264cbb
Matthew Morra <matt.morra@ozoralabs.com>
2020-09-30 04:18:11 +0800

    vco oscillations start

--- scripts/scomp.sh
+++ scripts/scomp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ..;
 rm -rf out/ csrc/ simv.daidir/ simv.msv/ simv ucli.key
-vcs -ams +ad -full64 -timescale=1ns/1ps nl.vams;
+vcs -ams +ad -full64 -override_timescale=1ns/1ps nl.vams;
 ./simv;
 
